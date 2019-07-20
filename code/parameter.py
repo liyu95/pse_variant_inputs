cd022de278ed05b2eabbb40b85193a583823dbab
Yu Li <yu.li@kaust.edu.sa>
2019-07-20 17:05:13 +0300

    update parameters

--- code/parameter.py
+++ code/parameter.py
@@ -24,25 +24,17 @@ output_step: The frequency of output the training acc
 	on the screen
 '''
 # run_id = args.id
-run_id = 'att_14_to_8'
+run_id = 'att_8_to_14'
 run_dir = run_id + '/'
 output_step = 10
 
 '''
 parameters related to the model configuration
-candidate model: densenet, resnet, senet, simple_conv
+candidate model: fc, attention, cnn
 weight_decay: weight regularizer coefficient
-params related to densenet:
-	layers: 40, 100, ...
-	growth: 12, 16, 32, ...
-params related to resnet:
-	blocks: 5 (32), 9 (56), 18 (110), 27(164)
-params related to se_resnet:
-	blocks: the same as the resnet
-	se_ratio: the squeeze ratio
 '''
 # model_name = args.model
-model_name = 'conv'
+model_name = 'attention'
 weight_decay = 0.0002
 
 
@@ -62,12 +54,10 @@ MCF_REG: whether use the MCF regularizer or not, it can be slow and
 batch_size = 8192
 test_batch = 8192
 epoches = 200
-init_lr = 0.01
-# optimized for densenet and resnet
-lr_dict = {10: 0.001, 20:0.0001}
+init_lr = 0.1
 keep_ratio = 0.8
 full_check = True
-less2more = False
+less2more = True
 
 
 '''
