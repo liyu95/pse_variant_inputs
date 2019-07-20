d60204466a628c0ccdc89d853721c11bc7ea3f99
Yu Li <yu.li@kaust.edu.sa>
2019-07-20 15:48:43 +0300

    update attention

--- code/attention_net.py
+++ code/attention_net.py
@@ -132,8 +132,8 @@ class EncoderLayer(tf.keras.layers.Layer):
 		self.mha = MultiHeadAttention(d_model, num_heads)
 		self.ffn = point_wise_feed_forward_network(d_model, dff)
 
-		# self.layernorm1 = tf.keras.layers.LayerNormalization(epsilon=1e-6)
-		# self.layernorm2 = tf.keras.layers.LayerNormalization(epsilon=1e-6)
+		self.layernorm1 = tf.keras.layers.LayerNormalization(epsilon=1e-6)
+		self.layernorm2 = tf.keras.layers.LayerNormalization(epsilon=1e-6)
 
 		self.dropout1 = tf.keras.layers.Dropout(rate)
 		self.dropout2 = tf.keras.layers.Dropout(rate)
@@ -142,13 +142,11 @@ class EncoderLayer(tf.keras.layers.Layer):
 
 		attn_output, _ = self.mha(x, x, x, mask)  # (batch_size, input_seq_len, d_model)
 		attn_output = self.dropout1(attn_output, training=training)
-		# out1 = self.layernorm1(x + attn_output)  # (batch_size, input_seq_len, d_model)
-		out1 = x + attn_output
+		out1 = self.layernorm1(x + attn_output)  # (batch_size, input_seq_len, d_model)
 
 		ffn_output = self.ffn(out1)  # (batch_size, input_seq_len, d_model)
 		ffn_output = self.dropout2(ffn_output, training=training)
-		# out2 = self.layernorm2(out1 + ffn_output)  # (batch_size, input_seq_len, d_model)
-		out2  = out1 + ffn_output
+		out2 = self.layernorm2(out1 + ffn_output)  # (batch_size, input_seq_len, d_model)
 
 		return out2
 
